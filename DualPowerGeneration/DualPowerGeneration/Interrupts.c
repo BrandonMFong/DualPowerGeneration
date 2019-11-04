24be76fd52feadc7d046b18543a1c6bd51b69c94
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2019-11-03 19:08:01 -0800

    fix

--- DualPowerGeneration/DualPowerGeneration/Interrupts.c
+++ DualPowerGeneration/DualPowerGeneration/Interrupts.c
@@ -1,3 +1,4 @@
+
 /*
  * CFile1.c
  *
@@ -53,7 +54,6 @@ ISR (TIMER0_COMPB_vect)
 	// Read Resistor 1 values
 }
 
-<<<<<<< HEAD
 /* Interrupt for ADC Converter
  * Tells us when we can get the value from the adc reg
  * NOTE: might not need this if I'm reading from the timer vector
@@ -72,19 +72,4 @@ ISR(ADC_vect)
 	 */
 	uint16_t AD = ADC;
 }
-=======
-
->>>>>>> fix
-
-
-
-
-
-
-
-
-
-
-
-
 
