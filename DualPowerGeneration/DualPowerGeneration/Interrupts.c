10ee58fdd2bf96597b382cc823f7d02f596cffc2
Joseph <joemorga4@gmail.com>
2019-11-08 15:06:22 -0800

    Added code to the interrupts

--- DualPowerGeneration/DualPowerGeneration/Interrupts.c
+++ DualPowerGeneration/DualPowerGeneration/Interrupts.c
@@ -21,24 +21,16 @@
 ISR (TIMER0_COMPA_vect)
 {
 	res0 = adc_read(CHANNEL_RESISTOR_0);
-	// Call your function here Joseph to move the solar panel
+	res1 = adc_read(CHANNEL_RESISTOR_1);
+	
+	if(res0 > res1) movePanelTo(WEST, res0, res1, &motorFactor_or_whatever);
+	else if(res1 > res0) movePanelTo(EAST, res1, res0, &motorFactor_or_whatever);
 	// Implement your values in Maneuver.c
 }
 
 ISR (TIMER0_COMPB_vect)
 {
-	res1 = adc_read(CHANNEL_RESISTOR_1);
-	// Call function
-}
-
-ISR (TIMER2_COMPA_vect)
-{
 	res2 = adc_read(CHANNEL_RESISTOR_2);
-	// Call function
-}
-
-ISR (TIMER2_COMPB_vect)
-{
 	res3 = adc_read(CHANNEL_RESISTOR_3);
 	// Call function
 	// Read Resistor 0 values
@@ -52,6 +44,10 @@ ISR (TIMER2_COMPB_vect)
 ISR (TIMER0_COMPB_vect)
 {
 	// Read Resistor 1 values
+	
+	if(res2 > res3) movePanelTo(NORTH, res2, res3, &motorFactor_or_whatever);
+	else if(res3 > res2) movePanelTo(SOUTH, res3, res2, &motorFactor_or_whatever);
+
 }
 
 /* Interrupt for ADC Converter
