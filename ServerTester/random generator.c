e51b37fc58c2d52aa4f5774dde3f0029b7dd5101
Lorans96 <56742056+Lorans96@users.noreply.github.com>
2019-11-09 16:15:27 -0800

    new average power
    updated average power random generator

--- ServerTester/random generator.c
+++ ServerTester/random generator.c
@@ -1,38 +1,39 @@
+ // calculate torque and angular velocity of the wind turbine
+ // take the average of the values to get the average power
+ // use equation P = torque* rpm*pi/30.
+ // torque in N/m and angular velocity in revolutions per minute
 #include <stdio.h>
 
 int main() {
    int i,total;
    //int srand[60];
    int n = 60;
-   int rpm(){
 int rand[60];
-   int total_rpm = 0;
+int srand[60];
+   total = 0;
 
    for(i = 0; i < n; i++) {
        int num;
-       num =  abs(rand[i] % (3600 + 1 - 0) + 0);
+       num =  abs(rand[i] % (2000 + 1 - 0) + 0);
 
-     total_rpm += num;
+     total += num;
       printf("%d\n", num);
    }
 
-   printf("Average rpm = %f\n", total_rpm/(float)n);
-   }
-   int torque(){
-   int srand[60];
-   int total_torque = 0;
-
-   for(int c = 0; c < n; c++) {
+   printf("Average rpm = %f\n", total/(float)n);
+   int total_1 = 0;
+     for(int c = 0; c < n; c++) {
        int num;
-       num =  abs(srand[c] % (1000 + 1 - 0) + 0);
+       num =  abs(srand[c] % (500 + 1 - 0) + 0);
 
-     total_torque += num;
+     total_1 += num;
+
       printf("%d\n", num);
    }
 
-   printf("Average torque = %f\n", total_torque/(float)n);
-   }
-
+  int pie = 3.14159265;
+    printf("Average torque = %f\n", total_1/(float)n);
+    printf("Average power for a minute = %f", total/(float)n*total_1/(float)n*pie/30);
 
    return 0;
 }
