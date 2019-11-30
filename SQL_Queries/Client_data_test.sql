d5fa631beb92c48a2950bae9ee929d0b0fd0ec81
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2019-11-30 12:19:33 -0800

    Adding untracked files

---
+++ SQL_Queries/Client_data_test.sql
@@ -0,0 +1,23 @@
+-- Making client
+
+insert into Client (Organization_Name, Admin_FirstName, Admin_LastName, ID)
+	values ('Org_0', 'Test0', 'User0', 1000);
+	
+-- Join for client and device
+insert into Device_Client (Device_ID, Client_ID)
+	values (1500, 1000);
+	
+-- Device table #5
+-- DATETIME - format: YYYY-MM-DD HH:MI:SS
+insert into Device (ID, Solar_ID, Wind_ID, Start_Date)
+	values (1500, 1501, 1502, '2019-11-30 12:00:00');
+	
+-- Solar TABLE #1
+insert into Solar (ID, Time)
+	values (1501, '2019-11-30 12:00:00');
+	
+-- Wind TABLE #2
+insert into Wind (ID, Time)
+	values (1502, '2019-11-30 12:00:00');
+	
+-- ID convention: Clientid-deviceid-solar-wind
