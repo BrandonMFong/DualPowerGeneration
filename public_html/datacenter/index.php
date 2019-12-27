50c80b15fabcff1bf7e154c9c41b3413f1221f22
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2019-12-26 22:54:33 -0800

    Needed to add a break in the case statement

--- public_html/datacenter/index.php
+++ public_html/datacenter/index.php
@@ -3,7 +3,7 @@
 
 <?php 
 	global $currpage;
-	$currpage = "Data Center"; // Include this to distinguish the page for the other php
+	$currpage = "DataCenter"; // Include this to distinguish the page for the other php
 	include '../php/header.php'; 
 	include '../php/sidenav.php'; 
 ?>
