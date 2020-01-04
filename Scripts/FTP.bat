507252fb9ce0a54731d4adcdc36fe19d28a33d88
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-01-04 12:48:36 -0800

    added %path:~0,-9% to allow bat file to run anywhere in a directory

--- Scripts/FTP.bat
+++ Scripts/FTP.bat
@@ -1,9 +1,13 @@
 @echo off
 
+set path=%~dp0
+echo "The path to DualPowerGeneration gitrepo:"
+echo %path:~0,-9% 
+
 "C:\Program Files (x86)\WinSCP\winscp.com" ^
   /command ^
     "open sftp://dualpower@146.244.101.140/ -hostkey=""ssh-ed25519 256 4o8fu3NLGOw4DARK0HJLAm5iu6LYt9o0sRFvu+eNF2M="" -privatekey=""B:\COLLEGE\19_20\Fall_19\CompE_496A\SSH\BrandonMFong.ppk"" -passphrase=""819295224bfWANG/""" ^
-    "lcd B:\COLLEGE\19_20\Fall_19\CompE_496A\DualPowerGeneration" ^
+    "lcd %path:~0,-9%" ^
     "cd /home/dualpower/public_ftp/incoming" ^
     "put FTP" ^
     "exit"
---
+++ Scripts/test.bat
@@ -0,0 +1,4 @@
+@echo off
+set path=%~dp0
+echo The path to DualPowerGeneration gitrepo:
+echo %path:~0,-9%
