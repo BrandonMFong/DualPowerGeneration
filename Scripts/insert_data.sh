179e66f07543f69880af83054584116786d7aa75
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-01-29 19:47:44 -0800

    Dev/max power tracker/feature/bf bash db script (#25)
    * [Test2]
    
    * [Test 3] forgot to init object
    
    * [Test 4]
    
    * [Test 5] adding if statement to rm cmd
    
    * [Test 6] syntax
    
    * [Test 7] filtering files
    
    * [Test 8] fixing mysql commands
    
    * [Test 9]

--- Scripts/insert_data.sh
+++ Scripts/insert_data.sh
@@ -3,13 +3,6 @@
 #This script is to only interact with the dualpower_DataCenter db 
 # The purpose of this script is to read the .csv file that was sent and insert into the database
 # refer to /DualPowerGeneration/MaxPowerTracker/example_file.csv for the column descriptions
-<<<<<<< HEAD
-
-# Local Test Notes:
-# I cannot connect to the mysql server with the socket in my.cnf, what the freeaaaaaak
-# look for /etc/mysql/my.cnf
-=======
->>>>>>> editting reading .csv file syntax
 
 # Local Test Notes:
 # I cannot connect to the mysql server with the socket in my.cnf, what the freeaaaaaak
