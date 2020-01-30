2331b47ee0bfabc197a23b96e27c53cd465c15f8
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-01-29 19:47:44 -0800

    Dev/max power tracker/feature/bf bash db script (#25)
    * [Test2]
    
    * [Test 3] forgot to init object
    
    * [Test 4]
    
    * [Test 5] adding if statement to rm cmd
    
    * [Test 6] syntax
    
    * [Test 7] filtering files
    
    * [Test 8] fixing mysql commands
    
    * [Test 9]

--- Scripts/insert_data.sh
+++ Scripts/insert_data.sh
@@ -19,20 +19,21 @@ Archive_dir="archive/"
 #FTP_dir="B:/SOURCES/Repos/DualPowerGeneration/FTP"; # Testing locally
 FTP_dir="/home/dualpower/public_ftp/incoming/FTP"; # On server
 
-pushd FTP_dir
+pushd $FTP_dir
         while [ $(find . -maxdepth 1 -type f|wc -l) -gt 0 ]; # keeps reading files in dir until there are not more files in dir
         do
                 # Testing if archive existss
-                if [ -d $dir ] # TODO figure out error
+                if ! [ -d $Archive_dir ] # TODO figure out error
                 then
-                        echo "$dir Exists/n";
-                else
-                        mkdir archive;
-                        echo "$dir does not exist.../nMade directory./n";
+                        mkdir $Archive_dir;
+                        echo "$Archive_dir does not exist...Made directory.";
                 fi
 
-                echo "Removing empty files";
-                rm $(find -empty); # Removes all files that are empty, this actually throws an error on the cmd line
+                if [ $(find . -empty -type f | wc -l) -gt 0 ] # checks for empty files
+                then
+                        echo "Removing empty files";
+                        rm $(find -empty -type f); # Removes all files that are empty, this actually throws an error on the cmd line
+                fi
 
                 current_working_file=$(find . -maxdepth 1  -type f | head -1);  # filters out the dir and filters files
                 echo "Currently reading file: $current_working_file";
@@ -41,13 +42,14 @@ pushd FTP_dir
                 while IFS=,  read -r Client_ID DateTime Max_Power_for_Wind Max_Power_for_Solar
                 do
                         # Query string
-                        query="set @Solar_ID = (select Solar_ID from client as cl join device_client as dc on cl.ID = dc.Client_ID join device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID); set @Wind_ID = (select Solar_ID from client as cl join device_client as dc on cl.ID = dc.Client_ID join device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID); insert into solar (ID,Time,Power) values (@Solar_ID, '$DateTime', $Max_Power_for_Solar); insert into wind (ID,Time,Power) values (@Wind_ID, '$DateTime', $Max_Power_for_Wind);";
-                        mysql -u$username -p$password -D$database -e$query # this is how you access the mysql terminal
+                        # This accesses the db correctly but it now says that client does not exist in the directory
+                        echo "set @Solar_ID = (select Solar_ID from client as cl join device_client as dc on cl.ID = dc.Client_ID join device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID); set @Wind_ID = (select Solar_ID from client as cl join device_client as dc on cl.ID = dc.Client_ID join device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID); insert into solar (ID,Time,Power) values (@Solar_ID, '$DateTime', $Max_Power_for_Solar); insert into wind (ID,Time,Power) values (@Wind_ID, '$DateTime', $Max_Power_for_Wind);";
+                     
                 # This should be a FIFO procedure for the files coming into the server
                 done < $current_working_file | mysql -u$username -p$password -D$database # might not need this mysql command
                 echo "Finished while loop for the insert query.";
 
-                mv $current_working_file $dir;
-                echo "Moved $current_working_file to $dir";
+                mv $current_working_file $Archive_dir;
+                echo "Moved $current_working_file to $Archive_dir";
         done 
 popd
