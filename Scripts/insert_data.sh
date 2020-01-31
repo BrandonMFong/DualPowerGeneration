a4e78b79c5ff792d4326f55bbec2f9f3a47595e1
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-01-31 15:18:14 -0800

    insert_data.sh is now able to read .csv files (#27)
    * edit insert_data.sh
    
    * editting reading .csv file syntax
    
    * Testing at server
    
    * editted script to test on server
    
    * [Test]
    
    * Dev/max power tracker/feature/bf bash db script (#25)
    
    * [Test2]
    
    * [Test 3] forgot to init object
    
    * [Test 4]
    
    * [Test 5] adding if statement to rm cmd
    
    * [Test 6] syntax
    
    * [Test 7] filtering files
    
    * [Test 8] fixing mysql commands
    
    * [Test 9]
    
    * insert_data.sh & Files.py
    
    * [Test2]
    
    * [Test 3] forgot to init object
    
    * [Test 4]
    
    * [Test 5] adding if statement to rm cmd
    
    * [Test 6] syntax
    
    * [Test 7] filtering files
    
    * [Test 8] fixing mysql commands
    
    * [Test 9]
    
    * [Test 1] Mariadb is case sensitive
    
    * [Test 2] Line continuation fix & Fixed @Wind_ID variable
    
    * [Test 3] ignoring python compiled files & testing to see if datetime variable is reading the datetime value in .csv file
    
    * [Test 4] testing datetime var
    
    * [Test 5] unqoting did not work, threw an error
    
    * [Test 6] notes & script to easily go on mariadb
    
    * [Test 7] fixed time format for output files to max MariaDB standards
    
    * [Test 8] establish infinite loop
    
    * [Test 9] reminder to clear logs\maxpower folder because all log files will be there. this might be a problem.
    
    * [Test 10] If-Else syntax error
    
    * [Test 11] edit permissions on Login MariaDB file

--- Scripts/insert_data.sh
+++ Scripts/insert_data.sh
@@ -1,54 +1,74 @@
-# This script is to only interact with the dualpower_DataCenter db 
+# Engineer: Brandon Fong
+#
+#This script is to only interact with the dualpower_DataCenter db 
 # The purpose of this script is to read the .csv file that was sent and insert into the database
-# refer to \DualPowerGeneration\MaxPowerTracker\example_file.csv for the column descriptions
+# refer to /DualPowerGeneration/MaxPowerTracker/example_file.csv for the column descriptions
+
+# Local Test Notes:
+# I cannot connect to the mysql server with the socket in my.cnf, what the freeaaaaaak
+# look for /etc/mysql/my.cnf
 
 # Important references:
 # http://lubos.rendek.org/import-data-from-csv-file-to-mysql-with-bash-script/
 # https://hoststud.com/resources/how-to-execute-mysql-queries-from-command-line-bash-shell.136/
+# https://www.cyberciti.biz/faq/unix-linux-bash-read-comma-separated-cvsfile/
 
+### VARIABLES ### 
 # credentials
 username="dualpower_BrandonMFong"
 password="dualpower27182"
 database="dualpower_DataCenter"
+Archive_dir="archive/"
+#FTP_dir="B:/SOURCES/Repos/DualPowerGeneration/FTP"; # Testing locally
+FTP_dir="/home/dualpower/public_ftp/incoming/FTP"; # On server
+DelayVar=5; # Delays for 5 seconds
 
-pushd /home/dualpower/public_ftp/incoming/FTP
-        while [ $(find . -maxdepth 1 -type f|wc -l) -gt 0 ]; # keeps reading files in dir until there are not more files in dir
-        do
-                # Testing if archive exists
-                dir=archive/
-                # if [ -d "$dir" ]
-                # then
-                #         echo "$dir Exists\n";
-                # else
-                #         mkdir archive;
-                #         echo "$dir does not exist...\nMade directory.\n";
-                # fi
-                # above does not seem to work
-                # it's fine if I made the directory myself, but if we are serving many different clients with different
-                # servers then it would be ideal to have these lines of code to work
-                # atm, not the priority
+### MAIN ###
+pushd $FTP_dir
+        while true 
+        do 
+                if [ $(find . -maxdepth 1 -type f|wc -l) -gt 0 ]; # keeps reading files in dir until there are not more files in dir
+                then
+                        # Testing if archive existss
+                        if ! [ -d $Archive_dir ] # TODO figure out error
+                        then
+                                mkdir $Archive_dir;
+                                echo "$Archive_dir does not exist...Made directory.";
+                        fi
 
-                echo "Removing empty files";
-                rm $(find -empty); # Removes all files that are empty, this actually throws an error on the cmd line
+                        if [ $(find . -empty -type f | wc -l) -gt 0 ] # checks for empty files
+                        then
+                                echo "Removing empty files";
+                                rm $(find -empty -type f); # Removes all files that are empty, this actually throws an error on the cmd line
+                        fi
 
-                current_working_file=$(find . -maxdepth 1  -type f | head -1);  # filters out the dir and filters files
-                echo "Currently reading file: $current_working_file";
+                        current_working_file=$(find . -maxdepth 1  -type f | head -1);  # filters out the dir and filters files
+                        echo "Currently reading file: $current_working_file";
 
-                # IFS is a way to say we are reading a comma delimited file
-                # This while loop reads the current file and works on inserting the data
-                IFS=, 
-                echo "Executing insert query";
-                while read Client_ID DateTime Max_Power_for_Wind Max_Power_for_Solar
-                do
-                        # Query string
-                        query="set @Solar_ID = (select Solar_ID from client as cl join device_client as dc on cl.ID = dc.Client_ID join device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID); set @Wind_ID = (select Solar_ID from client as cl join device_client as dc on cl.ID = dc.Client_ID join device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID); insert into solar (ID,Time,Power) values (@Solar_ID, '$DateTime', $Max_Power_for_Solar); insert into wind (ID,Time,Power) values (@Wind_ID, '$DateTime', $Max_Power_for_Wind);";
-                        mysql -u$username -p$password -D$database -e$query # this is how you access the mysql terminal
-                        exit;
-                # This should be a FIFO procedure for the files coming into the server
-                done < $current_working_file | mysql -u$username -p$password -D$database # might not need this mysql command
-                echo "Finished while loop for the insert query.";
+                        echo "Executing insert query";
+                        while IFS=,  read -r Client_ID DateTime Max_Power_for_Wind Max_Power_for_Solar
+                        do
+                                # Query string
+                                # This accesses the db correctly but it now says that client does not exist in the directory
+                                # echo "Date time variable";
+                                # echo $DateTime;
+                                querystring="set @Solar_ID = (select Solar_ID from Client as cl join Device_Client as dc";
+                                querystring+=" on cl.ID = dc.Client_ID join Device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID);";
+                                querystring+=" set @Wind_ID = (select Wind_ID from Client as cl join Device_Client as dc";
+                                querystring+=" on cl.ID = dc.Client_ID join Device as dev on dev.ID = dc.Device_ID where cl.ID = $Client_ID);";
+                                querystring+=" insert into Solar (ID,Time,Power) values (@Solar_ID, '$DateTime', $Max_Power_for_Solar);";
+                                querystring+=" insert into Wind (ID,Time,Power) values (@Wind_ID, '$DateTime', $Max_Power_for_Wind);";
+                                echo -n "${querystring}";
+                        
+                        # This should be a FIFO procedure for the files coming into the server
+                        done < $current_working_file | mysql -u$username -p$password -D$database # might not need this mysql command
+                        echo "Finished while loop for the insert query.";
 
-                mv $current_working_file $dir;
-                echo "Moved $current_working_file to $dir";
-        done 
+                        mv $current_working_file $Archive_dir;
+                        echo "Moved $current_working_file to $Archive_dir";
+                else
+                        echo "Waiting for more files...";
+                        sleep $DelayVar;
+                fi
+        done
 popd
