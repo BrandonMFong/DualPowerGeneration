c1bbbbe5d4dcf8521b785381954530240d06689c
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-02-08 16:24:16 -0800

    FTP by python

--- MaxPowerTracker/MaxPower/FTP.py
+++ MaxPowerTracker/MaxPower/FTP.py
@@ -4,11 +4,12 @@
 # Engineers: Lorans Hirmez, Brandon Fong            #
 #####################################################
 
-### LIBRARIES ###
+### LIBRARIES ### 
 from subprocess import call 
 from Files import Log_Handler
 from XML import xmlreader
 import subprocess, sys, os
+import ftplib
 
 global file_basename;
 file_basename = '..\\..\\Scripts\\FTP';
@@ -45,5 +46,10 @@ class FTP:
                 print(ex);
                 print("File not sent through ftp");
                 Log_Handler.Write_Log(os.path.basename(__file__) + "\n\n" + ex + "\n\n File not sent through cmd\n");
+        elif type == 'python':
+            print('do something');
+            # https://www.pythonforbeginners.com/code-snippets-source-code/how-to-use-ftp-in-python
+            # This is going to be for python method.  Doing this will relieve the worry of linux environment on rpi
+            # If I can ftp through here then that would be great  
         else:
             print("FTP procedure not defined.  Please check configuration on MaxPower.xml");
