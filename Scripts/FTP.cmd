891bdf83f48c58c1b77a468c4ab14a6efc8fc267
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-02-22 11:51:01 -0800

    Security breech

--- Scripts/FTP.cmd
+++
@@ -1,1 +0,0 @@
- /command "open sftp://dualpower@146.244.101.140/ -hostkey=""ssh-ed25519 256 4o8fu3NLGOw4DARK0HJLAm5iu6LYt9o0sRFvu+eNF2M="" -privatekey=""B:\COLLEGE\19_20\Fall_19\CompE_496A\SSH\BrandonMFong.ppk"" -passphrase=""819295224bfWANG/""" "lcd B:\COLLEGE\19_20\Fall_19\CompE_496A\DualPowerGeneration" "cd /home/dualpower/public_ftp/incoming" "put FTP" "exit"
