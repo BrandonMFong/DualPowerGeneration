d1cfe68a38ef8ab06eeee02b2c3e0b5cbfe81ad8
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-02-27 19:20:30 -0800

    setting infrared read method

--- MaxPowerTracker/MaxPower/IO.py
+++ MaxPowerTracker/MaxPower/IO.py
@@ -7,7 +7,7 @@
 # TODO use the following library to simulate the rotation of the blades to calculate rpm 
 # from pynput.keyboard import Key, Listener # https://pythonhosted.org/pynput/keyboard.html
 import RPi.GPIO as GPIO
-import pynput
+#import pynput
 import MaxPower_Classes
 import System
 import threading
@@ -16,17 +16,17 @@ from random import random
 
 xmlreader = xmlreader();
 # Below is just for simulation purposes
-class Keyboard_IO:
+#class Keyboard_IO:
 
-    def call_rpm(key):
-        if System.timer_flag: 
-            MaxPower_Classes.total_rpm = 0;
-            return exit();
-        MaxPower_Classes.Max_Power_Wind.Get_RPM(); # calls this function to increment
+#    def call_rpm(key):
+#        if System.timer_flag: 
+#            MaxPower_Classes.total_rpm = 0;
+#            return exit();
+#        MaxPower_Classes.Max_Power_Wind.Get_RPM(); # calls this function to increment
 
-    def RPM_Listener():
-        with Listener(on_press=Keyboard_IO.call_rpm) as input:
-            input.join()
+#    def RPM_Listener():
+#        with Listener(on_press=Keyboard_IO.call_rpm) as input:
+#            input.join()
 
 class Random_IO:
     def BLADE_FORCE_listener():
