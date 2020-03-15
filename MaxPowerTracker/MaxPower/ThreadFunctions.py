46488be30e39514215fe65045cd762fa6f1f74d8
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-15 14:45:32 -0700

    no use for the privatekey as of now

--- MaxPowerTracker/MaxPower/ThreadFunctions.py
+++ MaxPowerTracker/MaxPower/ThreadFunctions.py
@@ -18,6 +18,7 @@ System.init();
 MaxPower_Classes.init();
 
 # I do not want this thread to call any IO functions
+# I think I have to though because ReadInfrared is an async event
 def do():
     ### TIMER ### 
     THREAD_Timer = threading.Thread(target=System.timer);
