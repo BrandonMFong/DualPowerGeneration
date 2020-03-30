a5087349f4d7ada1b1f8954994b23ae343de3a39
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-03-29 18:29:16 -0700

    init config for settings.py

---
+++ YES-app/YES/YES/XML.py
@@ -0,0 +1,23 @@
+
+#####################################################
+# Property by Your Engineering Solutions (Y.E.S.)   #
+# Engineers: Lorans Hirmez, Brandon Fong            #
+#####################################################
+
+### LIBRARIES ###
+from xml.dom import minidom
+import xml.etree.ElementTree as ET # https://docs.python.org/3.3/library/xml.etree.elementtree.html
+
+CLIENT = 'EarthWindFire'; # TODO make this configurable
+XMLPath = '../../Config/' + CLIENT + '/MaxPower.xml';
+reader = minidom.parse(XMLPath);
+
+class xmlreader:
+    def int(self, value):
+        return int((reader.getElementsByTagName(value))[0].firstChild.data);
+
+    def string(self, value):
+        return str((reader.getElementsByTagName(value))[0].firstChild.data);
+
+    def double(self, value):
+        return double((reader.getElementsByTagName(value))[0].firstChild.data);
