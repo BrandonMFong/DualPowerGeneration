5f42dcefb503cbb62194cfa125469967c5e30eb1
Brandon Fong <55638760+BrandonMFong@users.noreply.github.com>
2020-04-05 18:19:02 -0700

    changing directory to looking at

---
+++ Scripts/Modules/KeyPressTest.psm1
@@ -0,0 +1,27 @@
+#Credits http://powershell.one/tricks/input-devices/detect-key-press
+function Test-KeyPress
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [ConsoleKey]
+        $Key,
+
+        [System.ConsoleModifiers]
+        $ModifierKey = 0
+    )
+    if ([Console]::KeyAvailable)
+    {
+        $pressedKey = [Console]::ReadKey($true)
+
+        $isPressedKey = $key -eq $pressedKey.Key
+        if ($isPressedKey)
+        {
+            return ($pressedKey.Modifiers -eq $ModifierKey);
+        }
+        else
+        {
+            return $false
+        }
+    }
+}
